668ae6c16fb87541ac04c4f8323c75fa6d62135a
SaraMsoso <Sara.soder44@gmail.com>
2026-02-13 10:55:54 +0100

    Added /register route to routing configuration

--- src/routing.tsx
+++ src/routing.tsx
@@ -5,6 +5,7 @@ import Posts from "./pages/Posts";
 import PostDetail from "./pages/PostDetail";
 import Admin from "./pages/Admin";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import Register from "./pages/Register";
 
 export default function Routing() {
   return (
@@ -12,6 +13,7 @@ export default function Routing() {
       {/* Public routes */}
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/posts" element={<Posts />} />
       <Route path="/posts/:id" element={<PostDetail />} />
 
